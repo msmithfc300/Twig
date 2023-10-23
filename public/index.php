43b36d24f42920df59fe51d0fc10bf05a7043b58
Martha Smith <m.e.smith@live.co.uk>
2023-10-23 22:22:14 +0200

    re-added the pages for quest1 oops

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
 
+
+$products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre', 'harpe'];
+
+echo $twig->render('products.html.twig', ['products' => $products]);
+
 echo $twig->render('home.html.twig');
 
 
